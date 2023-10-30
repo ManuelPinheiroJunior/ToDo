82f1e294f6ce55d0f9c363be48e69d1e093da5f5
Manuel Junior <manuel.jr@outlook.com>
2023-10-30 23:28:11 +0000

    DesSD

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors')
 const app = express()
-const sql = require('./db')
+const sql = require('./src/db')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
